d27e39c57e5bd76de80b9fec576c7aadf206c0e8
Simon Cropp <simon.cropp@gmail.com>
2013-12-04 15:30:02 +1100

    move to NSB 4.3
    still able to repro

--- NServiceBus 4.3 Bugs/1828/SubscribeHandler/DocumentSaga.cs
+++ NServiceBus 4.3 Bugs/1828/SubscribeHandler/DocumentSaga.cs
@@ -9,7 +9,7 @@ namespace SubscribeHandler
     {
         public void Handle(ILoanDocumentPublishedEvent message)
         {
-            Console.WriteLine("Loan Doument received -- {0}", message.Name);
+            Console.WriteLine("Loan Document received -- {0} {1}", message.Name, message.Type);
             Data.Name = message.Name;
             Data.IsLoanDocComplete = true;
             CompleteSaga();
@@ -26,7 +26,7 @@ namespace SubscribeHandler
 
         public void Handle(ILienDocumentPublishedEvent message)
         {
-            Console.WriteLine("Lien Doument received -- {0}", message.Name);
+            Console.WriteLine("Lien Document received -- {0} {1}", message.Name , message.Type);
            
             Data.Name = message.Name;
             Data.IsLienDocComplete = true;
