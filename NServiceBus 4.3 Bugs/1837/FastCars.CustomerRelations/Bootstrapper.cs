fc212016338e34964bc794b409f799153aa0e181
indualagarsamy <indu.alagarsamy@gmail.com>
2013-12-09 11:49:27 -0800

    Added a repro for https://github.com/Particular/NServiceBus/issues/1837

---
+++ NServiceBus 4.3 Bugs/1837/FastCars.CustomerRelations/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿namespace FastCars.CustomerRelations
+{
+    using System;
+    using NServiceBus;
+    using FastCars.Events;
+    public class Bootstrapper : IWantToRunWhenBusStartsAndStops
+    {
+        public IBus Bus { get; set; }
+        public void Start()
+        {
+            Console.WriteLine("Press any key to publish an event");
+            while (Console.ReadLine() != null)
+            {
+                Bus.Publish<ClientBecamePreferred>(m =>
+                {
+                    m.ClientId = Guid.NewGuid();
+                    m.PreferredUntil = DateTime.Now.AddDays(30);
+                });
+                Console.WriteLine("Published ClientBecamePreferred event");
+            }
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
