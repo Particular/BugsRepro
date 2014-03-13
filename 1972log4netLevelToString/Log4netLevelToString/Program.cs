581c10f97ec2dd146edc7f5c0e3f5d709a6b9a3d
Simon Cropp <simon.cropp@gmail.com>
2014-03-14 00:56:27 +1100

    show log4net tostring issue
    repro for https://github.com/Particular/NServiceBus/issues/1972

---
+++ 1972log4netLevelToString/Log4netLevelToString/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using log4net.Config;
+using NServiceBus;
+using NServiceBus.Installation.Environments;
+using NServiceBus.Unicast;
+
+class Program
+{
+
+    static void Main()
+    {
+        var bus = CreateBus();
+        Console.WriteLine("Press any key to exit");
+        Console.Read();
+        bus.Dispose();
+    }
+
+    static UnicastBus CreateBus()
+    {
+        Configure.GetEndpointNameAction = () => "IMutateIncomingMessagesIgnored";
+
+        XmlConfigurator.Configure();
+        var configure = Configure.With()
+            .Log4Net()
+            .DefaultBuilder()
+            .RavenSubscriptionStorage()
+            .UseRavenTimeoutPersister();
+        configure
+            .UnicastBus();
+
+        return (UnicastBus) configure.UnicastBus()
+            .CreateBus().Start(() => Configure.Instance.ForInstallationOn<Windows>().Install());
+    }
+
+}
