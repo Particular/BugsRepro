8a88d81c34380d8649186fadf805acb7351fb174
Simon Cropp <simon.cropp@gmail.com>
2014-09-05 20:57:49 +1000

    add stack trace repro

---
+++ StackTraceRepro/StackTraceRepro/MyHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NServiceBus;
+
+namespace StackTraceRepro
+{
+    class MyHandler:IHandleMessages<MyMessage>
+    {
+        public void Handle(MyMessage message)
+        {
+            ClassThaThrows.MethodThatThrows();
+        }
+    }
+
+    class ClassThaThrows
+    {
+        public static void MethodThatThrows()
+        {
+            MethodThatThrows2();
+        }
+
+        static void MethodThatThrows2()
+        {
+            throw new MyException("Foo Bar");
+        }
+    }
+
+    class MyException : Exception
+    {
+        public MyException(string message)
+            : base(message)
+        {
+            
+        }
+    }
+
+    class MyMessage:IMessage
+    {
+    }
+}
---
+++ StackTraceRepro/StackTraceRepro/ProgramService.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+using NServiceBus;
+using NServiceBus.Installation.Environments;
+using StackTraceRepro;
+
+class ProgramService : ServiceBase
+{
+    IStartableBus bus;
+
+    static void Main()
+    {
+        using (var service = new ProgramService())
+        {
+            // so we can run interactive from Visual Studio or as a service
+            if (Environment.UserInteractive)
+            {
+                service.OnStart(null);
+                Console.WriteLine("\r\nPress any key to stop program\r\n");
+                Console.Read();
+                service.OnStop();
+            }
+            else
+            {
+                Run(service);
+            }
+        }
+    }
+
+    protected override void OnStart(string[] args)
+    {
+        Configure.GetEndpointNameAction = () => "StackTraceRepro";
+        LoggingConfig.ConfigureLogging();
+
+        Configure.Serialization.Json();
+
+        bus = Configure.With()
+            .DefaultBuilder()
+            .InMemorySagaPersister()
+            .UseInMemoryTimeoutPersister()
+            .InMemorySubscriptionStorage()
+            .UnicastBus()
+            .CreateBus();
+        bus.Start(Startup);
+        bus.SendLocal(new MyMessage());
+    }
+
+    static void Startup()
+    {
+        //Only create queues when a user is debugging
+        if (Environment.UserInteractive && Debugger.IsAttached)
+        {
+            Configure.Instance.ForInstallationOn<Windows>().Install();
+        }
+    }
+
+    protected override void OnStop()
+    {
+        if (bus != null)
+        {
+            bus.Shutdown();
+        }
+    }
+}
